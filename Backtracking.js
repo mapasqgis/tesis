6792dbd55c8a88f6af06e395f64972a8edf8b92e
wally <lourdes.fernandezvallejos@gmail.com>
2020-04-15 21:58:40 -0300

    asd

---
+++ Backtracking.js
@@ -0,0 +1,19 @@
+//Backtracking
+  function back(origen,destino,linea[][]){
+      if(origen==destino){
+      }
+      else{
+      }
+  }
+
+//Suma de viajes
+  function sumaviajes(){
+  }
+
+//Suma de tiempos
+  function sumatiempo(){
+  }
+
+//Vecinos de origen
+  function vecinos(){
+  }
